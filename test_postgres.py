8bdf8f67bc65145a654800a402ea9b94ed7ef65f
devin <devin@devin.(none)>
2014-12-06 17:44:25 -0500

    test postgres

--- test_postgres.py
+++ test_postgres.py
@@ -3,6 +3,7 @@ import psycopg2
 import urlparse
 
 urlparse.uses_netloc.append("postgres")
+
 #url = urlparse.urlparse(os.environ["DATABASE_URL"])
 url = urlparse.urlparse('postgres://eevlzykkoizrya:3UV5kdzaj--jt21n3SFAdJbK1V@ec2-54-197-250-52.compute-1.amazonaws.com:5432/d5ermaa5dagmvt')
 
@@ -16,6 +17,8 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-print cur.execute("select * from information_schema.tables where table_schema = 'information_schema';")
+#cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+#conn.commit()
 
+cur.execute("select * from information_schema.tables where table_schema = 'public';")
+print cur.fetchall()[0]
